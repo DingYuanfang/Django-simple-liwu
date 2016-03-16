ce4828b7a6f19711f26dd3a6b6cb85dd94e3f76c
DingYuanfang <sixgod40@gmail>
2016-03-16 19:27:00 +0800

    Create README.md

---
+++ polls/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+
+#
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#
+# admin.site.register(Question, QuestionAdmin)
+# # ...
+# admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+#class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+admin.site.register(Question, QuestionAdmin)
